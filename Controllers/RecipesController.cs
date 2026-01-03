25b3d01fd4d1f93ebfd10496fad9916121270fb6
rahaf <rahaffallaha22@gmail.com>
2026-01-03 13:58:39 +0300

    doneee

--- Controllers/RecipesController.cs
+++ Controllers/RecipesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using WhiskItUp.Models;
 
 namespace WhiskItUp.Controllers
 {
+   // [Authorize]
     public class RecipesController : Controller
     {
         private readonly ApplicationDbContext _context;
---
+++ Models/ModelView/UserRecipesReport.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WhiskItUp.Models.ModelView
+{
+    public class UserRecipesReport
+    {
+        //Search By...
+        [Display(Name ="user id")]
+        public int UserId { get; set; }
+     
+        [EmailAddress]
+        [Display(Prompt = "Enter Your Email")]
+        public string? Email { get; set; }
+     
+        public EGender Gender { get; set; } = EGender.female;
+
+        //Results...
+        public IEnumerable<User> users { get; set; } = new List<User>();
+       
+
+    }
+}
