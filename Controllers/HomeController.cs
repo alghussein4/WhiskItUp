3038bd4ca004161e9eb395eeec5aa93e2411843a
rahaf <rahaffallaha22@gmail.com>
2026-01-06 10:13:57 +0300

    pationn done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,43 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using WhiskItUp.Data;
 using WhiskItUp.Models;
+using WhiskItUp.Models.ModelView;
+using WhiskItUp.Models.ViewModels;
 namespace WhiskItUp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-       
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            // ???? ???? 3 ????? ??? ????? ???????
+            var topRecipes = await _context.tblUserRecipeMapping
+         .Include(x => x.Recipe)
+         .GroupBy(x => x.Recipe)
+         .Select(g => new TopRatedRecipeVM
+         {
+             RecipeName = g.Key.RecipeName,  // ????? ?? Recipe
+             AvgRating = g.Average(x => x.Rating) // ??????? ?? UserRecipeMapping.Rating
+         })
+         .OrderByDescending(x => x.AvgRating)
+         .Take(3)
+         .ToListAsync();
+
+            return View(topRecipes); // ?????? ??? View
         }
+
+
         [Authorize]
         public IActionResult Privacy()
         {
